af47018011fbe9098579b89340ccef68118f9bc0
IevgenMalashchuk <ievgen.malashchuk@gmail.com>
2015-11-30 14:22:13 +0200

    Added "register_new_user" function declaration.

--- custom/service/v3_1_custom/SugarWebServiceImplv3_1_custom.php
+++ custom/service/v3_1_custom/SugarWebServiceImplv3_1_custom.php
@@ -7,6 +7,25 @@ if(!defined('sugarEntry'))define('sugarEntry', true);
 require_once 'service/v3_1/SugarWebServiceImplv3_1.php';
 class SugarWebServiceImplv3_1_custom extends SugarWebServiceImplv3_1 {
 
+	function register_new_user($session, $json)
+	{
+		$GLOBALS['log']->debug('Begin: register_new_user [start]');
+
+		//Here we check that $session represents a valid session
+		if (!self::$helperObject->checkSessionAndModuleAccess(
+			$session,
+			'invalid_session', '', '', '', new SoapError())
+		)
+		{
+			$GLOBALS['log']->error('End: register_new_user [error]');
+			return false;
+		}
+		$GLOBALS['log']->info($json);
+		// do the writing ...
+		$GLOBALS['log']->debug('End: register_new_user [finish]');
+		return true;
+	}
+
 	function write_user_data($session, $json)
 	{
 		$GLOBALS['log']->debug('Begin: write_user_data [start]');
@@ -14,17 +33,14 @@ class SugarWebServiceImplv3_1_custom extends SugarWebServiceImplv3_1 {
 		//Here we check that $session represents a valid session
 		if (!self::$helperObject->checkSessionAndModuleAccess(
 			$session,
-			'invalid_session',
-			'',
-			'',
-			'',
-			new SoapError())
+			'invalid_session', '', '', '', new SoapError())
 		)
 		{
 			$GLOBALS['log']->error('End: write_user_data [error]');
 			return false;
 		}
 		$GLOBALS['log']->info($json);
+		// do the writing ...
 		$GLOBALS['log']->debug('End: write_user_data [finish]');
 		return true;
 	}
