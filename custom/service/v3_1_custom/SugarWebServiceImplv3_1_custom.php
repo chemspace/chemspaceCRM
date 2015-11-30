6b989ce9ead6c661f4417c951cd1f4d485acae30
IevgenMalashchuk <ievgen.malashchuk@gmail.com>
2015-11-30 17:10:50 +0200

    "register_new_user" function implementation.

--- custom/service/v3_1_custom/SugarWebServiceImplv3_1_custom.php
+++ custom/service/v3_1_custom/SugarWebServiceImplv3_1_custom.php
@@ -15,13 +15,61 @@ class SugarWebServiceImplv3_1_custom extends SugarWebServiceImplv3_1 {
 		if (!self::$helperObject->checkSessionAndModuleAccess(
 			$session,
 			'invalid_session', '', '', '', new SoapError())
-		)
-		{
-			$GLOBALS['log']->error('End: register_new_user [error]');
+		) {
+			$GLOBALS['log']->error('End: register_new_user - session is invalid [error]');
 			return false;
 		}
-		$GLOBALS['log']->info($json);
-		// do the writing ...
+
+		// Check the data
+		$data = json_decode($json, true);
+		if (empty($data)) {
+			$GLOBALS['log']->error('End: register_new_user - data is empty [error]');
+			return false;
+		}
+		if (!is_array($data)) {
+			$GLOBALS['log']->error('End: register_new_user - data not array [error]');
+			return false;
+		}
+		$keys = array('user_id', 'email', 'salutation', 'firstname', 'lastname', 'country_id', 'currency', 'company_name', 'social_account', 'email_notification', 'reg_date');
+		foreach ($keys as $key) {
+			if (!isset($data[$key])) {
+				$GLOBALS['log']->error('End: register_new_user - data[' . $key . '] not set [error]');
+				return false;
+			}
+		}
+
+		// Do the writing ...
+		$bean = BeanFactory::newBean('Contacts');
+		$bean->description = 'Contact created via API.';
+
+		$bean->fe_user_id_c = strval($data['user_id']);
+		$bean->fe_email_c = strval($data['email']);
+
+		$bean->email1 = strval($data['email']);
+
+		$bean->salutation = strval($data['salutation']);
+		$bean->first_name = strval($data['firstname']);
+		$bean->last_name = strval($data['lastname']);
+
+		$bean->fe_salutation_c = strval($data['salutation']);
+		$bean->fe_first_name_c = strval($data['firstname']);
+		$bean->fe_last_name_c = strval($data['lastname']);
+
+		$bean->country_c = intval($data['country_id']);
+		$bean->fe_country_id_c = intval($data['country_id']);
+
+		$bean->currency_c = strval($data['currency']);
+		$bean->fe_currency_c = strval($data['currency']);
+
+		$bean->company_name_c = strval($data['company_name']);
+		$bean->fe_company_name_c = strval($data['company_name']);
+
+		$bean->fe_social_account_c = serialize($data['social_account']);
+		$bean->fe_email_notification_c = boolval($data['email_notification']);
+		$bean->fe_reg_date_c = strval($data['reg_date']);
+
+		$bean->save();
+
 		$GLOBALS['log']->debug('End: register_new_user [finish]');
 		return true;
 	}
